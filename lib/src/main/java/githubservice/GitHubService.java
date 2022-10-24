443b00feefa7bff87175d1dc4b104f5d876ba1bc
Michael Au <mau@synopsys.com>
2022-10-24 15:51:45 -0400

    Create commit and PR

--- lib/src/main/java/githubservice/GitHubService.java
+++ lib/src/main/java/githubservice/GitHubService.java
@@ -4,9 +4,7 @@ import org.kohsuke.github.*;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ListIterator;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class GitHubService {
     public static GitHub buildServiceViaLibrary(String accessToken) throws IOException {
@@ -15,39 +13,58 @@ public class GitHubService {
 
     public static void main(String[] args) {
         try {
+            // TODO: A bunch of strings to set to test
+            // GitHub, repo, files stuff
+            String accessToken = "";
+            String refBase = "refs/heads/";
+            String repoName = "mau-synopsys/test";
+            String fileName = "build.gradle";
+            // New change stuff
+            String newBranchName = "new-branch-2";
+            String newCommitMessage = "Commit 2";
+            String newTitle = "PR title 2";
+
+            // Dependency updates
             String dependency = "com.google.code.gson";
-            String version = "gson:2.9.1";
-            String newBranchName = "new-branch";
+            String currentVersion = "gson:2.9.1";   // Assumes versions are given. Make sure this matches for testing
+            String newVersion = "gson:2.9.0";
+
             // Get repo
-            GitHub gitHub = buildServiceViaLibrary("");
-            GHRepository ghRepository = gitHub.getRepository("mau-synopsys/test");
+            GitHub gitHub = buildServiceViaLibrary(accessToken);
+            GHRepository testRepository = gitHub.getRepository(repoName);
 
             // Branch off default, modify file, and commit
-            String defaultBranch = ghRepository.getDefaultBranch();
-            String defaultBranchSha = ghRepository.getRef("refs/heads/" + defaultBranch).getObject().getSha();
+            String defaultBranch = testRepository.getDefaultBranch();
+            String defaultBranchSha = testRepository.getRef(refBase + defaultBranch).getObject().getSha();
 
-            Map<String, GHBranch> branches = ghRepository.getBranches();
-            GHRef ghRef;
+            Map<String, GHBranch> branches = testRepository.getBranches();
+            GHRef newBranchRef;
             if (branches.containsKey(newBranchName)) {
-                ghRef = ghRepository.getRef("refs/heads/" + newBranchName);
+                newBranchRef = testRepository.getRef(refBase + newBranchName);
             } else {
-                ghRef = ghRepository.createRef("refs/heads/" + newBranchName, defaultBranchSha);
+                newBranchRef = testRepository.createRef(refBase + newBranchName, defaultBranchSha);
             }
 
-            GHContent ghContent = ghRepository.getFileContent("build.gradle", ghRef.getRef());
+            GHContent fileContentToModify = testRepository.getFileContent(fileName, newBranchRef.getRef());
 
             // Assume clean format
-            ListIterator<String> itr = (new String(ghContent.read().readAllBytes(), StandardCharsets.UTF_8))
-                    .lines()
-                    .collect(Collectors.toList())
-                    .listIterator();
+            String fileContentAsString = new String(fileContentToModify.read().readAllBytes(), StandardCharsets.UTF_8);
+            String newFileContentAsString = fileContentAsString.replace(dependency + ":" + currentVersion, dependency + ":" + newVersion);
 
-            while (itr.hasNext()) {
-                String s = itr.next();
-                if (s.contains(dependency)) {
-                    itr.set("\timplementation " + dependency + " " + version);
-                }
-            }
+            // Create the commit
+            GHCommit newBranchLatestCommit = testRepository.getCommit(newBranchRef.getObject().getSha());
+            GHTreeBuilder ghTreeBuilder = testRepository.createTree().baseTree(newBranchLatestCommit.getTree().getSha());
+            ghTreeBuilder.add(fileName, newFileContentAsString, false);
+            GHTree ghTree = ghTreeBuilder.create();
+            GHCommit updateDependencyVersionCommit = testRepository.createCommit()
+                    .parent(newBranchLatestCommit.getSHA1())
+                    .tree(ghTree.getSha())
+                    .message(newCommitMessage)
+                    .create();
+
+            // Create a PR from new to default branch
+            newBranchRef.updateTo(updateDependencyVersionCommit.getSHA1());
+            testRepository.createPullRequest(newTitle, newBranchRef.getRef(), refBase + defaultBranch, "new PR")
             ;
         } catch (IOException exception) {
             System.out.println((exception));
